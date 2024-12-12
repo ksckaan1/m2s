5625358bac676662f2625a8f63c2e56aa8e09489
Kaan Kuscu <me@kaanksc.com>
2024-12-12 22:59:25 +0300

    add custom error type for parsing errors

--- errors.go
+++ errors.go
@@ -8,3 +8,12 @@ var (
 	ErrValueMustBeStruct  = errors.New("value must be a struct")
 	ErrInvalidFieldType   = errors.New("invalid field type")
 )
+
+type ErrParseFailed struct {
+	Field string
+	Err   error
+}
+
+func (e ErrParseFailed) Error() string {
+	return "failed to parse field " + e.Field + ": " + e.Err.Error()
+}
